026eada188ad46a070b4fec2bcfe6a9e19d64dcc
Sakurahihih <23217391@life.hkbu.edu.hk>
2024-10-14 14:35:21 +0800

    Initial Commit

---
+++ Assignment1_IOS/Assignment1_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1_IOSApp.swift
+//  Assignment1_IOS
+//
+//  Created by Sam Liu on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
