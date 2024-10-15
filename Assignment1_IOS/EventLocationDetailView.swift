2b57f3871cebee836a9cd918d7a114baf6270e6b
Sakurahihih <23217391@life.hkbu.edu.hk>
2024-10-15 23:43:57 +0800

    Event finish

---
+++ Assignment1_IOS/EventLocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  EventLocationDetailView.swift
+//  Assignment1_IOS
+//
+//  Created by Sam Liu on 14/10/2024.
+//
+
+import SwiftUI
+
+struct EventLocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventLocationDetailView()
+}
